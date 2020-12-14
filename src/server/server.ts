4dc939a6fe08a7c5f1b8e3de0352a79648adc2ab
irajgreenberg <irajgreenberg@gmail.com>
2020-12-14 13:04:05 -0600

    changed server port for remote testing

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,7 @@ import express from "express"
 import path from "path"
 import http from "http"
 
-const port: number = 3000
+const port: number = 5000
 
 class App {
     private server: http.Server
@@ -15,13 +15,13 @@ class App {
         app.use('/build/three.module.js', express.static(path.join(__dirname, '../../node_modules/three/build/three.module.js')))
         app.use('/jsm/controls/OrbitControls', express.static(path.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')))
         app.use('/jsm/controls/OrbitControls', express.static(path.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')))
-        
+
         this.server = new http.Server(app);
     }
 
     public Start() {
         this.server.listen(this.port, () => {
-            console.log( `Server listening on port ${this.port}.` )
+            console.log(`Server listening on port ${this.port}.`)
         })
     }
 }
