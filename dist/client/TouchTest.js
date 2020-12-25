81a224b2eadb43785212332e6072f4da8d1ce12c
Ira <irajgreenberg@gmail.com>
2020-12-25 15:06:32 -0700

    New TouchTest enables placment of nodes, to incrementally construct organism

---
+++ dist/client/TouchTest.js
@@ -0,0 +1,169 @@
+//Boilerplate Typescript/Three/VSCode, by Sean Bradley:
+//git clone https://github.com/Sean-Bradley/Three.js-TypeScript-Boilerplate.git
+// For new projects:  1. npm install -g typescript, 
+//                    2. npm install
+// To run:            3. npm run dev 
+//                    Server runs locally at port 3000
+// These experiments support development
+// of an 'independent' softbody organism.
+// Work is being produced in collaboration with
+// Courtney Brown, Melanie Clemmons & Brent Brimhall
+// Draw a Pulsing Epidural Hood
+// contained within a cube
+// Original Author: Ira Greenberg, 11/2020
+// Center of Creative Computation, SMU
+//----------------------------------------------
+import * as THREE from '/build/three.module.js';
+import { OrbitControls } from '/jsm/controls/OrbitControls';
+import { VerletNode } from './VerletNode.js';
+import { GeometryDetail } from './IJGUtils.js';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.001, 2000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+document.addEventListener('click', onMouse, false);
+// cube bounds
+const bounds = new THREE.Vector3(2, 1.75, 1);
+// nodes
+let nodes = new Array(0);
+// // Create Epidermal Hood
+// // cover
+// let epidermalCover = new EpidermalHood(new THREE.Vector3(0, 0, 0), .27, .5, 30, 50, .875, [GeometryDetail.ICOSA, GeometryDetail.TETRA, GeometryDetail.TRI]);
+// epidermalCover.addHangingTendrils(10, .4, .9);
+// epidermalCover.addCilia(2, .05, .85);
+// epidermalCover.setDynamics(new Propulsion(new THREE.Vector3(0, 1, 0),
+//     new THREE.Vector3(0, -.04, 0),
+//     new THREE.Vector3(0, Math.PI / 1500, 0)));
+// epidermalCover.setMaterials(new VerletMaterials(
+//     new THREE.Color(.8, .6, 8),  /*node color*/
+//     new THREE.Color(.8, .1, .9), /*spine color*/
+//     .2,                          /*spine alpha*/
+//     new THREE.Color(.7, 0, .6),  /*slice color*/
+//     .5,                          /*slice alpha*/
+//     new THREE.Color(.7, .4, .6),  /*tendril node color*/
+//     new THREE.Color(.5, .3, .9),  /*tendril color*/
+//     .5,                          /*tendril alpha*/
+//     new THREE.Color(.5, .8, 1),  /*cilia node color*/
+//     new THREE.Color(.5, .8, 1),  /*cilia color*/
+//     .2));                        /*cilia alpha*/
+// epidermalCover.setNodesScale(6.2, 8, 3);
+// epidermalCover.setNodesVisible(true, true, true);
+// scene.add(epidermalCover);
+// let epidermalHood = new EpidermalHood(epidermalCover.getApex().multiply(new THREE.Vector3(-1)), .2, .2, 40, 30, .95);
+// epidermalHood.addHangingTendrils(20, .45, .79);
+// epidermalHood.setMaterials(new VerletMaterials(
+//     new THREE.Color(.9, .9, .9),
+//     new THREE.Color(.9, .7, .7),
+//     .4,
+//     new THREE.Color(1, .5, 0),
+//     .4,
+//     new THREE.Color(.8, .8, .9),
+//     new THREE.Color(.8, .8, .9),
+//     .2,
+//     new THREE.Color(.8, .8, .9),
+//     new THREE.Color(.8, .8, .9),
+//     .4));
+// epidermalHood.setNodesScale(4, 2);
+// epidermalHood.setNodesVisible(true, true, false);
+// scene.add(epidermalHood);
+// let epidermalHood2 = new EpidermalHood(new THREE.Vector3(0, 0, 0), .1, .1, 15, 12, .8);
+// epidermalHood2.addHangingTendrils(30, .55, .99);
+// epidermalHood2.setMaterials(new VerletMaterials(
+//     new THREE.Color(.3, 0, .1),
+//     new THREE.Color(.5, 0, .2),
+//     .8,
+//     new THREE.Color(.6, .2, 0),
+//     .8,
+//     new THREE.Color(.5, .2, .2),
+//     new THREE.Color(.6, .5, .5),
+//     .5,
+//     new THREE.Color(.8, .8, .9),
+//     new THREE.Color(.8, .8, .9),
+//     .4));
+// epidermalHood2.setNodesScale(10, 7);
+// epidermalHood2.setNodesVisible(true, true, false);
+// scene.add(epidermalHood2);
+// Create/add outer box
+const geometry2 = new THREE.BoxGeometry(bounds.x, bounds.y, bounds.z);
+const material2 = new THREE.MeshBasicMaterial({ color: 0x22ee00, wireframe: true });
+material2.transparent = true;
+material2.opacity = .08;
+const cube2 = new THREE.Mesh(geometry2, material2);
+scene.add(cube2);
+// Simple lighting calculations
+const color = 0xEEEEFF;
+const intensity = .65;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
+const color2 = 0xFFFFDD;
+const intensity2 = 1;
+const light2 = new THREE.DirectionalLight(color, intensity);
+light2.position.set(-2, 6, 1);
+//light2.target.position.set(0, 0, 0);
+scene.add(light2);
+//scene.add(light2.target);
+camera.position.y = .05;
+camera.position.z = 3;
+// animation vars
+// let spd: THREE.Vector3 = new THREE.Vector3(.01, .1, .1);
+// let theta = 0.0;
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
+}
+var animate = function () {
+    requestAnimationFrame(animate);
+    //controls.autoRotate = true;
+    camera.lookAt(scene.position); //0,0,0
+    // epidermalCover.pulse();
+    // epidermalCover.constrainBounds(bounds);
+    // epidermalHood.follow(epidermalCover.getApex().add(new THREE.Vector3(0, -.28, .0)));
+    // epidermalHood.constrainBounds(bounds);
+    // epidermalHood2.follow(epidermalHood.getApex().add(new THREE.Vector3(0, -.125, 0)));
+    // epidermalHood2.constrainBounds(bounds);
+    // camera.position.y = .05;
+    // camera.position.x = Math.cos(renderer.info.render.frame * Math.PI / 360) * .15;
+    // camera.position.y = Math.cos(renderer.info.render.frame * Math.PI / 720) * .15;
+    // camera.position.z = Math.sin(renderer.info.render.frame * Math.PI / 720) * .35;
+    updateNodes();
+    controls.update();
+    render();
+};
+function onMouse(event) {
+    // unproject algorithm from: WestLangley
+    // enables placement of nodes in world space based on mousepress (screen space placement)
+    // https://stackoverflow.com/questions/13055214/mouse-canvas-x-y-to-three-js-world-x-y-z
+    var vec = new THREE.Vector3(); // create once and reuse
+    var pos = new THREE.Vector3(); // create once and reuse
+    vec.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1, 0.5);
+    vec.unproject(camera);
+    vec.sub(camera.position).normalize();
+    var distance = -camera.position.z / vec.z;
+    pos.copy(camera.position).add(vec.multiplyScalar(distance));
+    addNode(pos);
+}
+function render() {
+    renderer.render(scene, camera);
+}
+animate();
+function addNode(pos) {
+    const n = new VerletNode(new THREE.Vector3(pos.x, pos.y, pos.z), THREE.MathUtils.randFloat(.01, .1), new THREE.Color(.7, .5, .7), GeometryDetail.DODECA);
+    nodes.push(n);
+    scene.add(n);
+    n.position.x += THREE.MathUtils.randFloatSpread(.02);
+    n.position.y += THREE.MathUtils.randFloatSpread(.02);
+    n.position.z += THREE.MathUtils.randFloatSpread(.02);
+}
+function updateNodes() {
+    if (nodes.length > 0) {
+        for (var i = 0; i < nodes.length; i++) {
+            // show nodes
+            nodes[i].verlet();
+        }
+    }
+}
